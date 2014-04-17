bb195a378bb2e7e93b817ae64558caddfa8012b7
Eugene Oh <eugeneoh@Eugenes-MacBook-Pro.local>
2014-04-16 22:31:30 -0400

    added shit that wasn't added before

--- js/results.js
+++ js/results.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+	// var buyBtn = document.getElementById("buy-button");
+	// $("#buy-button").click(function(){
+	// 	$(this).css("background-color","#E3E3E3");
+	// });
+	// $(".search-result").click(function(){
+	// 	var text = $(this).text();
+	// 	// var image = 
+	// });
 });
